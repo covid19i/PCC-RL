a1af5948ad461f823576e91e392dc19ed0c037b8
covid19i <r.ilyeech.k@gmail.com>
2021-05-07 13:53:52 +0530

    Code for bigger MLP models, LSTM model, loaded_agent.py modified to load LSTM models (mask, state variables)

---
+++ src/gym/stable_solve_LSTM_1loop.py
@@ -0,0 +1,141 @@
+# Copyright 2019 Nathan Jay and Noga Rotman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import gym
+import network_sim
+import tensorflow as tf
+
+from stable_baselines.common.policies import LstmPolicy
+#to try after LstmPolicy
+from stable_baselines.common.policies import MlpLstmPolicy, MlpLnLstmPolicy
+from stable_baselines import PPO1, PPO2
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+from common.simple_arg_parse import arg_or_default
+
+arch_str = arg_or_default("--arch", default="32,16")
+if arch_str == "":
+    arch = []
+else:
+    arch = [int(layer_width) for layer_width in arch_str.split(",")]
+print("Architecture for vf and pi is: %s" % str(arch))
+#64, 64 is (sort of) the default for the shared layers in LstmPolicy according to 
+#https://stable-baselines.readthedocs.io/en/master/_modules/stable_baselines/common/policies.html#LstmPolicy
+#Also, LSTMs are not supported in the shared part
+net_arch = [64, 64, 'lstm', {"pi":arch, "vf":arch}]
+lstm_dim = 64
+print("Overall architecture is: %s" % str(net_arch))
+print("LSTM dimenstion: %s" % str(lstm_dim))
+
+training_sess = None
+
+#/home/ubuntu/environments/my_env/lib/python3.5/site-packages/stable_baselines/common/policies.py
+#https://stable-baselines.readthedocs.io/en/master/guide/custom_policy.html
+class MyLstmPolicy(LstmPolicy):
+
+    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm=lstm_dim, reuse=False, **_kwargs):
+        #layer_norm = True gives the following error:
+        #Traceback (most recent call last):
+        #File "stable_solve_LSTM.py", line 26, in <module>
+        #from caffe2.python.helpers.normalization import layer_norm
+        #ImportError: No module named 'caffe2'
+        super(MyLstmPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm, reuse, 
+                                          layers = None, net_arch=net_arch, layer_norm = False, 
+                                          feature_extraction="mlp", **_kwargs)
+        global training_sess
+        training_sess = sess
+
+#https://stable-baselines.readthedocs.io/en/master/guide/custom_policy.html
+class CustomLSTMPolicy(LstmPolicy):
+    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm=lstm_dim, reuse=False, **_kwargs):
+        super().__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm, reuse,
+                         net_arch=[8, 'lstm', dict(vf=[5, 10], pi=[10])],
+                         layer_norm=True, feature_extraction="mlp", **_kwargs)
+        global training_sess
+        training_sess = sess
+
+
+env = gym.make('PccNs-v0')
+print("Number of environments used for training (env.num_envs): " + str(env.num_envs))
+#env = gym.make('CartPole-v0')
+
+gamma = arg_or_default("--gamma", default=0.99)
+print("gamma = %f" % gamma)
+#https://github.com/hill-a/stable-baselines/#implemented-algorithms
+#PPO1 can't be used with Recurrent
+#In Algo 1 of paper: T is timesteps_per_actorbatch, M = optim_batchsize << NT.
+#model = PPO1(MyLstmPolicy, env, verbose=1, schedule='constant', timesteps_per_actorbatch=8192, optim_batchsize=2048, gamma=gamma)
+#https://stable-baselines.readthedocs.io/en/master/modules/ppo2.html?highlight=ppo2
+model = PPO2(MyLstmPolicy, env, verbose=1, nminibatches = 1, n_steps=20, gamma=gamma)
+
+
+#Stable Baselines 3 tutorial
+#https://github.com/araffin/rl-tutorial-jnrr19
+for i in range(0, 1):
+    with model.graph.as_default():                                                                   
+        saver = tf.train.Saver()                                                     
+        saver.save(training_sess, "./pcc_model_%d.ckpt" % i)
+    model.learn(total_timesteps=(16 * 40))
+
+##
+#   Save the model to the location specified below.
+##
+default_export_dir = "/home/ubuntu/models/"
+export_dir = arg_or_default("--model-dir", default=default_export_dir)
+with model.graph.as_default():
+
+    #https://stable-baselines.readthedocs.io/en/master/_modules/stable_baselines/ppo2/ppo2.html#PPO2
+    pol = model.act_model#act_model
+
+    obs_ph = pol.obs_ph
+    act = pol.deterministic_action
+    sampled_act = pol.action#??
+    
+    train = model.train_model
+    #param masks: (np.ndarray) The last masks for done episodes (used in recurent policies)
+    mask_ph = train.dones_ph#masks
+    #param states: (np.ndarray) For recurrent policies, the internal state of the recurrent model
+    states_ph = train.states_ph
+    
+
+    obs_input = tf.saved_model.utils.build_tensor_info(obs_ph)
+    outputs_tensor_info = tf.saved_model.utils.build_tensor_info(act)
+    stochastic_act_tensor_info = tf.saved_model.utils.build_tensor_info(sampled_act)
+    mask_ph_tensor_info = tf.saved_model.utils.build_tensor_info(mask_ph)
+    #Looks fishy. REMOVE THIS IF IT DOESN'T WORK.
+    #mask_ph_tensor_info.name = mask_ph_tensor_info.name.replace("train_model/", "")
+    states_ph_tensor_info = tf.saved_model.utils.build_tensor_info(states_ph)
+    #Looks fishy. REMOVE THIS IF IT DOESN'T WORK.
+    #states_ph_tensor_info.name = states_ph_tensor_info.name.replace("train_model/", "")
+    signature = tf.saved_model.signature_def_utils.build_signature_def(
+        inputs={"ob":obs_input, "state": states_ph_tensor_info, "mask": mask_ph_tensor_info},
+        outputs={"act":outputs_tensor_info, "stochastic_act":stochastic_act_tensor_info},
+        method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME)
+    print("Signature saved: " + str(signature))
+
+    #"""
+    signature_map = {tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
+                     signature}
+
+    model_builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
+    model_builder.add_meta_graph_and_variables(model.sess,
+        tags=[tf.saved_model.tag_constants.SERVING],
+        signature_def_map=signature_map,
+        clear_devices=True)
+    model_builder.save(as_text=True)#as_text = True not to be done in Prod
--- src/udt-plugins/testing/loaded_agent.py
+++ src/udt-plugins/testing/loaded_agent.py
@@ -20,37 +20,54 @@ class LoadedModel():
 
     def __init__(self, model_path):
         self.sess = tf.Session()
+        #print("self.sess: " + str(self.sess))
         self.model_path = model_path
         self.metagraph = tf.saved_model.loader.load(self.sess,
             [tf.saved_model.tag_constants.SERVING], self.model_path)
+        #print("self.metagraph: " + str(self.metagraph))
         sig = self.metagraph.signature_def["serving_default"]
+        print("Signature of the model: " + str(sig))
         input_dict = dict(sig.inputs)
-        output_dict = dict(sig.outputs)       
+        #print("input_dict of the model: " + str(input_dict))
+        output_dict = dict(sig.outputs)    
+        #print("output_dict of the model: " + str(output_dict))   
  
         self.input_obs_label = input_dict["ob"].name
+        print("input_obs_label: " + str(self.input_obs_label))
         self.input_state_label = None
         self.initial_state = None
         self.state = None
         if "state" in input_dict.keys():
-            self.input_state_label = input_dict["state"].name
+            #replace looks fishy. Remove if it doesn't work.
+            self.input_state_label = input_dict["state"].name.replace("train_model/", "")
             strfile = io.StringIO()
             print(input_dict["state"].tensor_shape, file=strfile)
+            print("state: shape:")
+            print(input_dict["state"].tensor_shape)
             lines = strfile.getvalue().split("\n")
             dim_1 = int(lines[1].split(":")[1].strip(" "))
             dim_2 = int(lines[4].split(":")[1].strip(" "))
+            print("dim_1: " + str(dim_1) + "\tdim_2: " + str(dim_2) + "\n")
             self.initial_state = np.zeros((dim_1, dim_2), dtype=np.float32)
-            self.state = np.zeros((dim_1, dim_2), dtype=np.float32)
+            self.state = np.zeros((1, 128), dtype=np.float32)
+        else:
+            print("Exiting because state couldn't be found with a Recurrent Policy in use.")
+            sys.exit()
  
         self.output_act_label = output_dict["act"].name
+        print("output_act_label: " + str(self.output_act_label))
         self.output_stochastic_act_label = None
         if "stochastic_act" in output_dict.keys():
             self.output_stochastic_act_label = output_dict["stochastic_act"].name
+        print("stochastic_act_label: " + str(self.output_stochastic_act_label))
 
         self.mask = None
         self.input_mask_label = None 
         if "mask" in input_dict.keys():
-            self.input_mask_label = input_dict["mask"].name
-            self.mask = np.ones((1, 1)).reshape((1,))
+            #replace looks fishy. Remove if it doesn't work.
+            self.input_mask_label = input_dict["mask"].name.replace("train_model/", "")
+            self.mask = np.ones(( 1 ))
+        self.printed_input_dict = 0
 
     def reset_state(self):      
         self.state = np.copy(self.initial_state)
@@ -61,11 +78,16 @@ class LoadedModel():
  
     def act(self, obs, stochastic=False):
         input_dict = {self.input_obs_label:obs}
+        #input_dict = {self.input_obs_label:obs}
+        #print("in LoadedModel.act() input_dict: \n" + str(input_dict))
         if self.state is not None:
             input_dict[self.input_state_label] = self.state
 
         if self.mask is not None:
             input_dict[self.input_mask_label] = self.mask
+        if(self.printed_input_dict == 0):
+            print("in LoadedModel.act() input_dict after state, mask added: \n" + str(input_dict))
+            self.printed_input_dict = 1
 
         sess_output = None
         if stochastic:
@@ -91,7 +113,9 @@ class LoadedModelAgent():
         self.model.reset_state()
 
     def act(self, ob):
-
+        #stochastic = False???
+        if(self.model.printed_input_dict == 0):
+            print("observations: " + str(ob))
         act_dict = self.model.act(ob.reshape(1,-1), stochastic=False)
 
         ac = act_dict["act"]
