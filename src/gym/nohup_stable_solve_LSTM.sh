53032b967c59d5fa379d3eec5b5c8fe2648166fa
covid19i <r.ilyeech.k@gmail.com>
2021-05-09 01:05:17 +0530

    nohup_stable_solve_LSTM.sh with SHELL variables

--- src/gym/nohup_stable_solve_LSTM.sh
+++ src/gym/nohup_stable_solve_LSTM.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
-cd /home/ubuntu/duplicate-for-training/PCC-RL/src/gym
+cd /home/ubuntu/PCC-RL/src/gym
 source /home/ubuntu/environments/my_env/bin/activate
-nohup python3 stable_solve_LSTM.py --model-dir=/home/ubuntu/models/LSTM_nminibatch_1_n_steps_2048_loops_6_lstm_dim_256/ > ./LSTM_nminibatch_1_n_steps_2048_loops_6_lstm_dim_256.txt
+
+RUN_NAME="LSTM_run2_1600x410_2048_1_lstm_dim_64"
+echo $RUN_NAME
+echo $$
+echo "File Name: $0"
+
+python3 stable_solve_LSTM.py --model-dir=/home/ubuntu/models/$RUN_NAME/ > ./$RUN_NAME.txt
+
+mkdir $RUN_NAME
+mv ./$RUN_NAME.txt $RUN_NAME
+mv pcc_model_* pcc_env_log_run_.json $RUN_NAME
