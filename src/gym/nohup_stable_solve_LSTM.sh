f60a892af8280c4e25fe1146ded252c320457f0e
covid19i <r.ilyeech.k@gmail.com>
2021-05-09 18:32:29 +0530

    automatic arguments

--- src/gym/nohup_stable_solve_LSTM.sh
+++ src/gym/nohup_stable_solve_LSTM.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 source /home/ubuntu/environments/my_env/bin/activate
-
-RUN_NAME="LSTM_run2_1600x410_2048_1_lstm_dim_64"
-echo $RUN_NAME
 echo $$
 echo "File Name: $0"
-
-python3 stable_solve_LSTM.py --model-dir=/home/ubuntu/models/$RUN_NAME/ > ./$RUN_NAME.txt
+RUN_NAME="LSTM_run$1_1600x410_loops_$2_timesteps_$3_1_lstm_dim_$4"
+echo "RUN_NAME: $RUN_NAME"
+python3 stable_solve_LSTM.py --model-dir=/home/ubuntu/models/$RUN_NAME/ --lstm_dim=$4 --no_of_timesteps=$3 --no_of_training_loops=$2 --RUN_ID=LSTM_run$1 > ./$RUN_NAME.txt
 
 mkdir $RUN_NAME
 mv ./$RUN_NAME.txt $RUN_NAME
-mv pcc_model_* pcc_env_log_run_.json $RUN_NAME
+mv pcc_* $RUN_NAME
+#RUN_NAME="LSTM_run4_1600x410_2048_1_lstm_dim_64"
